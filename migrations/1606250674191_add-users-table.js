cb7a60923ebfc06c0f9678628e9e826c7e1bf212
John Fields <jjfields24@outlook.com>
2020-11-24 15:58:43 -0500

    users migration code

--- migrations/1606250674191_add-users-table.js
+++ migrations/1606250674191_add-users-table.js
@@ -2,6 +2,21 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
+    pgm.sql(`
+      CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+          username VARCHAR(30) NOT NULL,
+          email VARCHAR(100) NOT NULL,
+          password VARCHAR(50) NOT NULL
+      );
+    `);
+};
 
-exports.down = pgm => {};
+exports.down = pgm => {
+    pgm.sql(`
+      DROP TABLE users;
+    `)
+};
