3fde1db294cb18e97072409473c9c5d2e890cb64
John Fields <jjfields24@outlook.com>
2020-11-24 17:15:54 -0500

    added close and query methods

--- pool.js
+++ pool.js
@@ -8,6 +8,14 @@ class Pool {
         // Return statement for testing purposes
         return this._pool.query('SELECT 1 + 1;');
     }
+
+    close() {
+      return this._pool.end();
+    }
+
+    query(sql) {
+      return this._pool.query(sql);
+    }
 }
 
 module.exports = new Pool();
