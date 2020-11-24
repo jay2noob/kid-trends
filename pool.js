cfdde26a5a72349554a198a2bd922ee926e44ed3
John Fields <jjfields24@outlook.com>
2020-11-24 16:57:23 -0500

    added query to test server and pool

--- pool.js
+++ pool.js
@@ -5,6 +5,8 @@ class Pool {
 
     connect(options) {
         this._pool = new pg.Pool(options);
+        // Return statement for testing purposes
+        return this._pool.query('SELECT 1 + 1;');
     }
 }
 
